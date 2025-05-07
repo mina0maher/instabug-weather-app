2540abd2cf14d8ebbcd01f7311f383ba32fc9b35
mina0maher <mahermena160@gmail.com>
2025-05-07 21:03:22 +0300

    Delete Unused imports

--- app/src/main/java/com/mina/weather/domain/usecase/GetIncomingDaysForecastUseCase.kt
+++ app/src/main/java/com/mina/weather/domain/usecase/GetIncomingDaysForecastUseCase.kt
@@ -1,14 +1,11 @@
 package com.mina.weather.domain.usecase
 
-import com.mina.weather.domain.models.DayForecast
+
 import com.mina.weather.domain.models.LatLng
 import com.mina.weather.domain.repository.WeatherRepository
 import com.mina.weather.domain.utils.extensions.getDayName
 import com.mina.weather.domain.utils.extensions.toCelsius
 import com.mina.weather.domain.utils.helpers.tryToExecute
-import com.mina.weather.domain.utils.states.Result
-import java.io.IOException
-import java.net.SocketTimeoutException
 
 class GetIncomingDaysForecastUseCase(
     private val weatherRepository: WeatherRepository
