f0dbe697b5b23e0113c41a2464b35e5d6583da55
mina0maher <mahermena160@gmail.com>
2025-05-07 20:13:14 +0300

    Edit Icons

--- app/src/main/java/com/mina/weather/presentation/ui/utils/helpers/getImageSource.kt
+++ app/src/main/java/com/mina/weather/presentation/ui/utils/helpers/getImageSource.kt
@@ -4,12 +4,22 @@ import com.mina.weather.R
 
 fun getImageSource(weatherDescription: String): Int {
     return when (weatherDescription.lowercase()) {
-        "clear" -> R.drawable.sun
-        "partly cloudy" -> R.drawable.cloudy_sunny
+        "clear-day" -> R.drawable.sun
+
+        "clear-night" , "partly-cloudy-night" -> R.drawable.clear_night
+
+        "partly-cloudy-day" -> R.drawable.cloudy_sunny
+
         "cloudy" -> R.drawable.cloudy
-        "rain" -> R.drawable.rainy
-        "snow" -> R.drawable.snowy
-        "storm" -> R.drawable.storm
+
+        "rain", "showers-day", "showers-night" -> R.drawable.rainy
+
+        "snow", "snow-showers-day", "snow-showers-night" -> R.drawable.snowy
+
+        "thunder-rain", "thunder-showers-day", "thunder-showers-night" -> R.drawable.storm
+
+        "fog", "wind" -> R.drawable.cloudy
+
         else -> R.drawable.cloudy_sunny
     }
 }
